72b994ea81df27e01bfa8aca35d3334c8ca006cb
Nrup <nruppatel2004@gmail.com>
2025-12-09 07:58:19 +0530

    chore: setup Next.js + Tailwind v3 + Webpack environment stable

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  // forces Next.js to run Webpack instead of Turbopack
+  experimental: {
+    webpackBuildWorker: false
+  }
+};
+
+export default nextConfig;
